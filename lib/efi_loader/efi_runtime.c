openharmony/device_hisilicon_third_party_uboot

commit ecae89e44d22de90fc77808c514cf873524e0a6b

efi_loader: detach runtime in ExitBootServices()
Linux can be called with a command line parameter efi=novamap, cf.
commit 4e46c2a95621 ("efi/arm/arm64: Allow SetVirtualAddressMap() to be
omitted"). In this case SetVirtualAddressMap() is not called after
ExitBootServices().

OpenBSD 32bit does not call SetVirtualAddressMap() either.

Runtime services must be set to an implementation supported at runtime
in ExitBootServices().

Reported-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
Suggested-by: Alexander Graf <agraf@csgraf.de>
Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

lib/efi_loader/efi_runtime.c
@@ -398,7 +398,7 @@ static bool efi_is_runtime_service_pointer(void *p)
 /**
  * efi_runtime_detach() - detach unimplemented runtime functions
  */
-static __efi_runtime void efi_runtime_detach(void)
+void efi_runtime_detach(void)
 {
 	efi_runtime_services.reset_system = efi_reset_system;
 	efi_runtime_services.get_time = efi_get_time;

include/efi_loader.h
@@ -333,6 +333,8 @@ void efi_variables_boot_exit_notify(void);
 efi_status_t efi_root_node_register(void);
 /* Called by bootefi to initialize runtime */
 efi_status_t efi_initialize_system_table(void);
+/* efi_runtime_detach() - detach unimplemented runtime functions */
+void efi_runtime_detach(void);
 /* Called by bootefi to make console interface available */
 efi_status_t efi_console_register(void);
 /* Called by bootefi to make all disk storage accessible as EFI objects */

lib/efi_loader/efi_boottime.c
@@ -1973,6 +1973,9 @@ static efi_status_t EFIAPI efi_exit_boot_services(efi_handle_t image_handle,
 
 	board_quiesce_devices();
 
+	/* Patch out unsupported runtime function */
+	efi_runtime_detach();
+
 	/* Fix up caches for EFI payloads if necessary */
 	efi_exit_caches();
 

openharmony/third_party_u-boot

commit e3f20504f70077a9322495abb2ccd5b6bbe4ebaf

efi_loader: do not call efi_runtime_detach twice
Commit ecae89e44d22 ("efi_loader: detach runtime in ExitBootServices()")
added a call to efi_runtime_detach() to ExitBootServices() but did not
remove the call in SetVirtualAddressMap().

Remove the superfluous function call.

Correct a comment referring to efi_runtime_detach().

Fixes: ecae89e44d22 ("efi_loader: detach runtime in ExitBootServices()")
Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

lib/efi_loader/efi_runtime.c
@@ -565,7 +565,10 @@ void efi_runtime_relocate(ulong offset, struct efi_mem_desc *map)
 
 		p = (void*)((ulong)rel->offset - base) + gd->relocaddr;
 
-		/* The runtime services are updated in efi_runtime_detach() */
+		/*
+		 * The runtime services table is updated in
+		 * efi_relocate_runtime_table()
+		 */
 		if (map && efi_is_runtime_service_pointer(p))
 			continue;
 
@@ -723,14 +726,6 @@ static efi_status_t EFIAPI efi_set_virtual_address_map(
 			systab.tables = (struct efi_configuration_table *)ptr;
 		}
 	}
-
-	/*
-	 * Some runtime services are implemented in a way that we can only offer
-	 * them at boottime. Replace those function pointers.
-	 *
-	 * TODO: move this call to ExitBootServices().
-	 */
-	efi_runtime_detach();
 
 	/* Relocate the runtime. See TODO above */
 	for (i = 0; i < n; i++) {