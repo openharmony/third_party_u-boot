u-boot/u-boot

xilinx: Add sd boot command script for reference
This patch adds sdboot command script for reference.
This can be converetd into uboot script using mkimage and
use for booting.

Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
Signed-off-by: Michal Simek <michal.simek@xilinx.com>

board/xilinx/bootscripts/sdboot.cmd
@@ -1,0 +1,10 @@
+# This is an example file to generate boot.scr - a boot script for U-Boot
+# This exmaple only target for qspi boot, sameway it can be created for boot
+# devices like nand.
+# Generate boot.scr:
+# ./tools/mkimage -c none -A arm -T script -d sdboot.cmd boot.scr
+#
+# It requires a list of environment variables used below to be defined
+# before load
+#
+mmc dev $sdbootdev && mmcinfo && run uenvboot || run sdroot$sdbootdev;load mmc $sdbootdev:$partid $fdt_addr system.dtb && load mmc $sdbootdev:$partid $kernel_addr Image && booti $kernel_addr - $fdt_addr

openharmony/third_party_u-boot

board/xilinx/bootscripts/sdboot.cmd
@@ -1,0 +1,10 @@
+# This is an example file to generate boot.scr - a boot script for U-Boot
+# This example only target for qspi boot, sameway it can be created for boot
+# devices like nand.
+# Generate boot.scr:
+# ./tools/mkimage -c none -A arm -T script -d sdboot.cmd boot.scr
+#
+# It requires a list of environment variables used below to be defined
+# before load
+#
+mmc dev $devnum && mmcinfo && run uenvboot || run sdroot$devnum;load mmc $devnum:$partid $fdt_addr system.dtb && load mmc $devnum:$partid $kernel_addr Image && booti $kernel_addr - $fdt_addr